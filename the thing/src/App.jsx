7daa5acca809cd080641e4b6ed8c1731f3ce5eb0
Hrushal-Nikhare <hrushalnikhare@gmail.com>
2023-02-24 18:11:56 +0530

    I call it a day

--- the thing/src/App.jsx
+++ the thing/src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import ScriptTag from 'react-script-tag';
 
-export default function App() {
+function App() {
   const [count, setCount] = useState(0)
 
   return (
@@ -27,8 +28,26 @@ export default function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.1.9/p5.min.js"></script>
+      <script src="https://cdn.jsdelivr.net/npm/vanta@latest/dist/vanta.topology.min.js"></script>
+      <div id="hee"></div>
+      <script>
+        VANTA.TOPOLOGY({
+        el: "#hee",
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        scale: 1.00,
+        scaleMobile: 1.00,
+        color: 0x31a8b1
+        })
+      </script>
+      
     </div>
+
   )
 }
 
-//export default App
+export default App
